a084ec884474ba20c2fcb12441b21cd8014f9f78
Andrew Watson <58689469+Dotsworthy@users.noreply.github.com>
2020-10-02 21:16:14 +0100

    initial commit

---
+++ src/main/java/com/example/bpdts/BPDTSTEST/BpdtstestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bpdts.BPDTSTEST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpdtstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpdtstestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bpdts/BPDTSTEST/BpdtstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bpdts.BPDTSTEST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpdtstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
