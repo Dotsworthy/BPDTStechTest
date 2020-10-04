a1822e725b31f0a82747f0d0a58fae25a666a6be
Andrew Watson <58689469+Dotsworthy@users.noreply.github.com>
2020-10-04 17:19:30 +0100

    createAPI now finds users roughly 60 miles from London

--- src/main/java/com/example/bpdts/BPDTSTEST/components/CreateAPI.java
+++ src/main/java/com/example/bpdts/BPDTSTEST/components/CreateAPI.java
@@ -15,6 +15,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 @Component
 public class CreateAPI implements ApplicationRunner {
@@ -24,17 +25,21 @@ public class CreateAPI implements ApplicationRunner {
 
     public void run(ApplicationArguments args) {
         getApi("London", urlLondon);
+        getApi("ArrayList", urlAllUsers);
+
     }
 
     @Autowired
     UserRepository userRepository;
+    ArrayList<JSONObject> filteredUsers = new ArrayList<JSONObject>();
 
     URL urlLondon = new URL("https://bpdts-test-app-v3.herokuapp.com/city/London/users");
     URL urlAllUsers = new URL("https://bpdts-test-app-v3.herokuapp.com/users");
 
+
     public static HttpURLConnection connection;
 
-    public void getApi(String apiLocation, URL url) {
+    public void getApi(String getType, URL url) {
         BufferedReader reader;
         String line;
         StringBuffer responseContent = new StringBuffer ();
@@ -43,8 +48,8 @@ public class CreateAPI implements ApplicationRunner {
             connection = (HttpURLConnection) url.openConnection();
 
             connection.setRequestMethod("GET");
-            connection.setConnectTimeout(5000);
-            connection.setReadTimeout(5000);
+//            connection.setConnectTimeout(5000);
+//            connection.setReadTimeout(5000);
 
             int status = connection.getResponseCode();
 
@@ -61,7 +66,14 @@ public class CreateAPI implements ApplicationRunner {
                 }
                 reader.close();
             }
-            parseLondoners(responseContent.toString());
+            if (getType == "London") {
+                parseLondoners(responseContent.toString());
+            } else if (getType == "ArrayList") {
+                parseUsersByProximity(responseContent.toString());
+            } else if (getType == "User") {
+                return;
+            }
+
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -87,7 +99,6 @@ public class CreateAPI implements ApplicationRunner {
 
             userRepository.save(api);
         }
-
     }
 
     public void parseUsersByProximity(String responseBody) {
@@ -100,6 +111,32 @@ public class CreateAPI implements ApplicationRunner {
             String ipAddress = user.getString("ip_address");
             double latitude = user.getDouble("latitude");
             double longitude = user.getDouble("longitude");
+            String city = "not London";
+
+            if (latitude >= 50.6374 && latitude <= 52.3774 && longitude >= -0.7122 && longitude <= 0.9978) {
+               filteredUsers.add(user);
+//                User api = new User(firstName, lastName, email, ipAddress, latitude, longitude, city);
+//                userRepository.save(api);
+            }
+        }
+    }
+
+    public void parseUser(String responseBody) {
+        JSONArray users = new JSONArray(responseBody);
+        for (int i = 0; i < users.length(); i++) {
+            JSONObject user = users.getJSONObject(i);
+            String firstName = user.getString("first_name");
+            String lastName = user.getString("last_name");
+            String email = user.getString("email");
+            String ipAddress = user.getString("ip_address");
+            double latitude = user.getDouble("latitude");
+            double longitude = user.getDouble("longitude");
+            String city = user.getString("city");
+
+            if (city != "London") {
+                User api = new User(firstName, lastName, email, ipAddress, latitude, longitude, city);
+                userRepository.save(api);
+            }
         }
     }
 }
