0a61516dbe16f336c517a96de064e72c12f9ad9e
Andrew Watson <58689469+Dotsworthy@users.noreply.github.com>
2020-10-05 11:27:11 +0100

    added route for city

--- src/main/java/com/example/bpdts/BPDTSTEST/controllers/UserController.java
+++ src/main/java/com/example/bpdts/BPDTSTEST/controllers/UserController.java
@@ -1,42 +1,29 @@
 package com.example.bpdts.BPDTSTEST.controllers;
 
-import com.example.bpdts.BPDTSTEST.models.User;
 import com.example.bpdts.BPDTSTEST.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value="/users")
 public class UserController {
 
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping(value = "/users")
-    public List<User> getAllUsers() {
-
-        return userRepository.findAll();
-    }
-
-    @GetMapping(value = "/users/{id}")
-    public Optional<User> getUser(@PathVariable Long id) {
-
-        return userRepository.findById(id);
-    }
-
-    @GetMapping(value = "/{city}/users")
-    public ResponseEntity<List<User>> findUsersFilterByCity(
-            @RequestParam(name = "city", required = false) String city) {
-        if (city != null) {
-            return new ResponseEntity<>(userRepository.findUserByCity(city), HttpStatus.OK);
+    @GetMapping
+    public ResponseEntity getAllUsersAndFilters(
+            @RequestParam(name="city", required=false) String city,
+            @RequestParam(name="id", required=false) Long id
+        ){
+        if (id != null) {
+           return new ResponseEntity(userRepository.findUserById(id), HttpStatus.OK);
         }
-        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+        if (city != null) {
+            return new ResponseEntity(userRepository.findUserByCity(city), HttpStatus.OK);
+        }
+        return new ResponseEntity(userRepository.findAll(), HttpStatus.OK);
     }
 }
--- src/main/java/com/example/bpdts/BPDTSTEST/repositories/UserRepository.java
+++ src/main/java/com/example/bpdts/BPDTSTEST/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.bpdts.BPDTSTEST.repositories;
 
 import com.example.bpdts.BPDTSTEST.models.User; // NEW
 import org.springframework.data.jpa.repository.JpaRepository; // NEW
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findUserByCity(String city);
+
+    List<User> findUserById(Long id);
 }
