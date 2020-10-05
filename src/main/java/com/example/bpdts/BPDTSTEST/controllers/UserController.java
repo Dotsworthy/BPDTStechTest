a796fb1655dd75dd2ded6af540af36d61f27fe51
Andrew Watson <58689469+Dotsworthy@users.noreply.github.com>
2020-10-05 10:59:03 +0100

    added basic route for city(doesn't work)

--- src/main/java/com/example/bpdts/BPDTSTEST/controllers/UserController.java
+++ src/main/java/com/example/bpdts/BPDTSTEST/controllers/UserController.java
@@ -3,8 +3,11 @@ package com.example.bpdts.BPDTSTEST.controllers;
 import com.example.bpdts.BPDTSTEST.models.User;
 import com.example.bpdts.BPDTSTEST.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,11 +21,22 @@ public class UserController {
 
     @GetMapping(value = "/users")
     public List<User> getAllUsers() {
+
         return userRepository.findAll();
     }
 
     @GetMapping(value = "/users/{id}")
     public Optional<User> getUser(@PathVariable Long id) {
+
         return userRepository.findById(id);
+    }
+
+    @GetMapping(value = "/{city}/users")
+    public ResponseEntity<List<User>> findUsersFilterByCity(
+            @RequestParam(name = "city", required = false) String city) {
+        if (city != null) {
+            return new ResponseEntity<>(userRepository.findUserByCity(city), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
     }
 }
--- src/main/java/com/example/bpdts/BPDTSTEST/repositories/UserRepository.java
+++ src/main/java/com/example/bpdts/BPDTSTEST/repositories/UserRepository.java
@@ -4,6 +4,9 @@ import com.example.bpdts.BPDTSTEST.models.User; // NEW
 import org.springframework.data.jpa.repository.JpaRepository; // NEW
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findUserByCity(String city);
 }
